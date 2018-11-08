0eac1b88d3b540f5af00639bb8d39109043629f7
bkunz53 <43080736+bkunz53@users.noreply.github.com>
2018-11-08 17:20:52 -0500

    Updated Scripts

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -5,24 +5,50 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class Prospector : MonoBehaviour {
+public class Prospector : MonoBehaviour
+{
+    static public Prospector S;
 
-	static public Prospector 	S;
+    [Header("Set in Inspector")]
+    public TextAsset deckXML;
+    public TextAsset layoutXML;
 
-	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+    [Header("Set Dynamically")]
+    public Deck deck;
+    public Layout layout;
+    public List<CardProspector> drawPile;
 
+    void Awake()
+    {
+        S = this; // Set up a Singleton for Prospector 
+    }
 
-	[Header("Set Dynamically")]
-	public Deck					deck;
+    void Start()
+    {
+        deck = GetComponent<Deck>();   // Get the Deck 
+        deck.InitDeck(deckXML.text);   // Pass DeckXML to it  
+        Deck.Shuffle(ref deck.cards);  // This shuffles the deck 
 
-	void Awake(){
-		S = this;
-	}
+        // This section can be commented out; we're working on real layout now 
+        //        Card c; 
+        //        for (int cNum=0; cNum<deck.cards.Count; cNum++) { 
+        //            c = deck.cards[cNum]; 
+        //            c.transform.localPosition = new Vector3((cNum%13)*3,cNum/13*4,0);
+        //        } 
 
-	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
-	}
-
+        layout = GetComponent<Layout>();  // Get the Layout component 
+        layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it 
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;                   // a 
+            lCP.Add(tCP);
+        }
+        return (lCP);
+    }
 }
